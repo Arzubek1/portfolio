7b8f34feb06e1a1e466a1f644282d01079a5e1c8
Arzubek1 <arzubek729@gmail.com>
2025-09-26 19:14:24 +0600

    orbita

--- src/components/pages/skils/Skils.tsx
+++ src/components/pages/skils/Skils.tsx
@@ -1,18 +1,33 @@
 import { FC } from "react";
 import scss from "./Skils.module.scss";
 import OpenSkills from "./openSkills/OpenSkills";
-
-
+import Image from "next/image";
 
 const Skils: FC = () => {
   return (
     <section className={scss.skils}>
       <div className="container">
-          <h2>Skills</h2>
+        <h2>Skills</h2>
         <div className={scss.content}>
-<div className={scss.top}>
-  <OpenSkills />
-</div>
+          <div className={scss.top}>
+            <OpenSkills />
+          </div>
+          <div className={scss.mainContent}>
+            <div className={scss.mainCircle}>
+              <span></span>
+              <div className={scss.js}><h3>JS</h3></div>
+              <div className={scss.html}><Image src={"/images/html.webp"} alt="img" width={56} height={56}/></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/components/pages/skils/openSkills/OpenSkills.tsx
+++ src/components/pages/skils/openSkills/OpenSkills.tsx
@@ -2,31 +2,29 @@ import React from "react";
 import scss from "./OpenSkills.module.scss";
 import Image from "next/image";
 
-const skills = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Redux/Toolkit",
-  "Sass",
-  "TypeScript",
-  "Next.js",
-  "GitHub",
-  "Framer Motion",
-  "Tailwind",
-];
-
 const OpenSkills = () => {
   return (
     <div className={scss.openSkills}>
-      <div className="container">
-          <h4> <Image src={"/images/openSkills_image.png"} alt="img" width={50} height={50}/> Open skills</h4>
-        <div className={scss.content}>
-            {skills.map((skill, index) => (
-                <div key={index} className={scss.skillItem}>
-                    {skill}
-                </div>
-            ))}
+      <h4>
+        Technical Skills
+      </h4>
+      <div className={scss.content}>
+        <div className={scss.skillItem}>
+          <h5><span>01.</span>HTML</h5>
+          <h5><span>02.</span>CSS</h5>
+          <h5><span>03.</span>JavaScript</h5>
+          <h5><span>04.</span>React</h5>
+        </div>
+        <div className={scss.skillItem}>
+          <h5><span>05.</span>Redux/Toolkit</h5>
+          <h5><span>06.</span>SASS</h5>
+          <h5><span>07.</span>TypeScript</h5>
+          <h5><span>08.</span>NextJS</h5>
+        </div>
+        <div className={scss.skillItem}>
+          <h5><span>09.</span>GitHub</h5>
+          <h5><span>10.</span>Tailwind</h5>
+          <h5><span>11.</span>Framer Motion</h5>
         </div>
       </div>
     </div>
