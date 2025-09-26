39d1adf4a3c92bbd8d796028a12a2489d514a0fa
Arzubek1 <arzubek729@gmail.com>
2025-09-27 00:27:02 +0600

    chat

--- src/components/pages/skils/Skils.tsx
+++ src/components/pages/skils/Skils.tsx
@@ -1,75 +1,114 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
 import scss from "./Skils.module.scss";
 import OpenSkills from "./openSkills/OpenSkills";
 import Image from "next/image";
+import GptBlock from "./gptBlock/GptBlock";
 
-const blockTop = [
-  <Image src={"/images/css.webp"} alt="img" />,
-  <Image src={"/images/html.jpg"} alt="img" />,
-  <Image src={"/images/js.avif"} alt="img" />,
-  <Image src={"/images/react.avif"} alt="img" />,
-  <Image src={"/images/ts.png"} alt="img" />,
-  <Image src={"/images/next.jpg"} alt="img" />,
-  <Image src={"/images/sass.png"} alt="img" />,
-  <Image src={"/images/github.jpg"} alt="img" />,
-  <Image src={"/images/node.png"} alt="img" />,
+interface SkillItem {
+  src: string;
+  keyword: string;
+  description: string;
+}
+
+const blockTop: SkillItem[] = [
+  { src: "/images/css.webp", keyword: "CSS", description: "CSS – веб үчүн стилдөө тили." },
+  { src: "/images/html.jpg", keyword: "HTML", description: "HTML – веб үчүн структуралык тил." },
+  { src: "/images/js.avif", keyword: "JavaScript", description: "JS – веб үчүн негизги программалоо тили." },
+  { src: "/images/react.avif", keyword: "React", description: "React – UI китепканасы." },
+  { src: "/images/ts.png", keyword: "TypeScript", description: "TS – JSке статикалык типтерди кошот." },
+  { src: "/images/next.jpg", keyword: "Next.js", description: "Next – React үчүн SSR/SSG фреймворк." },
+  { src: "/images/sass.png", keyword: "Sass", description: "Sass – CSS препроцессор." },
+  { src: "/images/github.jpg", keyword: "GitHub", description: "GitHub – кодду сактоо жана версиялоо." },
+  { src: "/images/node.png", keyword: "Node.js", description: "Node – серверде JS иштетет." },
 ];
-const blockBottom = [
-  <Image src={"/images/express.png"} alt="img" />,
-  <Image src={"/images/mongo.png"} alt="img" />,
-  <Image src={"/images/tailwind.webp"} alt="img" />,
-  <Image src={"/images/swagger.webp"} alt="img" />,
-  <Image src={"/images/vite.png"} alt="img" />,
-  <Image src={"/images/prisma.jpg"} alt="img" />,
-  <Image src={"/images/fastify.png"} alt="img" />,
-  <Image src={"/images/postman.png"} alt="img" />,
-  <Image src={"/images/insomnia.png"} alt="img" />,
+
+const blockBottom: SkillItem[] = [
+  { src: "/images/express.png", keyword: "Express", description: "Express – Node үчүн web framework." },
+  { src: "/images/mongo.png", keyword: "MongoDB", description: "Mongo – NoSQL база." },
+  { src: "/images/tailwind.webp", keyword: "Tailwind", description: "Tailwind – utility-first CSS." },
+  { src: "/images/swagger.webp", keyword: "Swagger", description: "Swagger – API документациясы." },
+  { src: "/images/vite.png", keyword: "Vite", description: "Vite – frontend dev сервер." },
+  { src: "/images/prisma.jpg", keyword: "Prisma", description: "Prisma – ORM Node.js үчүн." },
+  { src: "/images/fastify.png", keyword: "Fastify", description: "Fastify – Node.js framework." },
+  { src: "/images/postman.png", keyword: "Postman", description: "Postman – API тестер." },
+  { src: "/images/insomnia.png", keyword: "Insomnia", description: "Insomnia – API client." },
 ];
 
 const Skils: FC = () => {
-  
+  const [hoverData, setHoverData] = useState<{
+    visible: boolean;
+    keyword: string;
+    description: string;
+    image: string;
+    position: { x: number; y: number };
+  }>({
+    visible: false,
+    keyword: "",
+    description: "",
+    image: "",
+    position: { x: 0, y: 0 },
+  });
+
+  const handleMouseEnter = (e: React.MouseEvent, item: SkillItem) => {
+    const rect = (e.target as HTMLElement).getBoundingClientRect();
+    setHoverData({
+      visible: true,
+      keyword: item.keyword,
+      description: item.description,
+      image: item.src,
+      position: { x: rect.right + 10, y: rect.top },
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setHoverData(prev => ({ ...prev, visible: false }));
+  };
+
   return (
     <section className={scss.skils}>
       <h2>Skills</h2>
       <div className={scss.content}>
-        <div className={scss.top}>
-          <OpenSkills />
-        </div>
+<div className={scss.top}>
+  <OpenSkills />
+  
+  <GptBlock
+    visible={hoverData.visible}
+    keyword={hoverData.keyword}
+    description={hoverData.description}
+    image={hoverData.image}
+     position={{x:0, y:0}}
+  />
+</div>
+
+
         <div className={scss.mainContent}>
-          <div className={scss.mainTop}>
-
-            {blockTop.map((img, index) => (
-
-              <div className={scss.img} key={index} >
-                <Image
-                  src={img.props.src}
-                  alt={img.props.alt}
-                  width={320} // px
-                  height={120} // px
-                  style={{ objectFit: "cover" }}
-                />
-              </div>
-          
-            ))}
-      
-
-
+          {/* Жогорку ряд (оңго жылат) */}
+          <div className={scss.slider}>
+            <div className={`${scss.track} ${scss.right}`}>
+              {blockTop.concat(blockTop).map((item, i) => (
+                <div key={i} onMouseEnter={(e) => handleMouseEnter(e, item)} onMouseLeave={handleMouseLeave}>
+                  <Image src={item.src} alt={item.keyword} width={320} height={120} />
+                </div>
+              ))}
+            </div>
           </div>
-          <div className={scss.mainBottom}>
-            {blockBottom.map((img, index) => (
-              <div className={scss.img} key={index}>
-                <Image
-                  src={img.props.src}
-                  alt={img.props.alt}
-                  width={320} // px
-                  height={120} // px
-                  style={{ objectFit: "cover" }}
-                />
-              </div>
-            ))}
+
+          {/* Төмөнкү ряд (солго жылат) */}
+          <div className={scss.slider}>
+            <div className={`${scss.track} ${scss.left}`}>
+              {blockBottom.concat(blockBottom).map((item, i) => (
+                <div key={i} onMouseEnter={(e) => handleMouseEnter(e, item)} onMouseLeave={handleMouseLeave}>
+                  <Image src={item.src} alt={item.keyword} width={320} height={120} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
+
+     
+    
     </section>
   );
 };
